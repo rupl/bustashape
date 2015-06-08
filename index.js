54a8d55234e8f5bf923479500adf574d690cdb8f
Chris Ruppel <chris.ruppel@gmail.com>
2015-06-08 16:23:54 +0100

    :link: better comments for sockets

--- index.js
+++ index.js
@@ -13,28 +13,40 @@ app.get('/', function(req, res){
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 
-// Sockets
+/**
+ * Someone connected.
+ */
 io.on('connection', function(socket){
-  console.log('a user connected');
-  io.emit('status', 'someone joined');
+  console.log('user: connected');
 
+  /**
+   * A new shape appears!
+   */
   socket.on('add', function(props){
-    console.log('shape added', props);
+    console.log('shape: added', props);
     io.emit('add', props);
   });
 
+  /**
+   * A shape is being dragged.
+   */
   socket.on('move', function(props){
-    console.log('shape moved', props);
+    console.log('shape: moved', props);
     io.emit('move', props);
   });
 
+  /**
+   * Someone got bored.
+   */
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('user: disconnected');
     io.emit('status', 'someone left');
   });
 });
 
-// Listen for users to connect
+/**
+ * Listen for users to connect
+ */
 http.listen(3000, function(){
-  console.log('listening on port 3000');
+  console.log('Listening on port 3000');
 });
