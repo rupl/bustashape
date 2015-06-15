20c5ca74937d565c6dfc26c49d0838ea46578b8f
Chris Ruppel <chris.ruppel@gmail.com>
2015-06-15 20:33:51 +0200

    :book: commenting and re-organizing

--- js/client.js
+++ js/client.js
@@ -1,5 +1,14 @@
 var socket = io();
+var reqAnimationFrame = (function () {
+  return window[Hammer.prefixed(window, 'requestAnimationFrame')] || function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
 
+/**
+ * Add a new shape on all clients. It doesn't immediately add a shape to your
+ * DOM, the 'add' listener below handles that part.
+ */
 $('#add').click(function(e){
   socket.emit('add', {
     id: 'shape-' + Math.floor(Math.random() * 1000000000),
@@ -13,28 +22,28 @@ $('#add').click(function(e){
   e.preventDefault();
 });
 
+/**
+ * Listen for new additions and add them to DOM.
+ */
 socket.on('add', function(props) {
+  // Add the new element
   $('#canvas').append($('<div>')
     .addClass('shape')
     .attr('id', props.id)
     .css(props)
   );
 
-  var reqAnimationFrame = (function () {
-    return window[Hammer.prefixed(window, 'requestAnimationFrame')] || function (callback) {
-      window.setTimeout(callback, 1000 / 60);
-    };
-  })();
-
-  var log = document.querySelector("#log");
+  // Set up Hammer event listeners
   var el = document.querySelector('#' + props.id);
-
+  var initAngle = 0;
+  var initScale = 1;
   var timer;
   var ticking = false;
   var transform = {
-    translate: { x: 0, y: 0 },
-    scale: 1,
-    angle: 0,
+    x: 0,
+    y: 0,
+    scale: initScale,
+    angle: initAngle,
     rx: 0,
     ry: 0,
     rz: 0
@@ -43,35 +52,50 @@ socket.on('add', function(props) {
   var mc = new Hammer.Manager(el);
 
   mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
-
-  mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
   mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
   mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith([mc.get('pan'), mc.get('rotate')]);
-
   mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
   mc.add(new Hammer.Tap());
 
   mc.on("panstart panmove", onPan);
   mc.on("rotatestart rotatemove", onRotate);
   mc.on("pinchstart pinchmove", onPinch);
-  mc.on("swipe", onSwipe);
   mc.on("tap", onTap);
   mc.on("doubletap", onDoubleTap);
-
   mc.on("hammer.input", function(ev) {
     if(ev.isFinal) {
-      syncElement();
+      requestElementUpdate();
     }
   });
 
+  /**
+   * Easily pull coords out of a transform string.
+   */
+  function extract(css) {
+    var coords = css.match(/translate3d\((\d+)px, (\d+)px/);
 
-  function syncElement() {
-    requestElementUpdate();
+    return {
+      x: coords[1],
+      y: coords[2]
+    };
   }
 
+  /**
+   * Paint the changes.
+   */
+  function requestElementUpdate() {
+    if(!ticking) {
+      reqAnimationFrame(updateElementTransform);
+      ticking = true;
+    }
+  }
+
+  /**
+   * rAF callback which paints to the screen and emits the movement to others.
+   */
   function updateElementTransform() {
     var value = [
-      'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0px)',
+      'translate3d(' + transform.x + 'px, ' + transform.y + 'px, 0px)',
       'scale(' + transform.scale + ', ' + transform.scale + ')',
       'rotate3d('+ transform.rx +','+ transform.ry +','+ transform.rz +','+  transform.angle + 'deg)'
     ];
@@ -82,6 +106,7 @@ socket.on('add', function(props) {
     el.style.transform = value;
     ticking = false;
 
+    // Send position to everyone else.
     socket.emit('move', {
       me: socket.id,
       id: props.id,
@@ -89,96 +114,88 @@ socket.on('add', function(props) {
     });
   }
 
-  function requestElementUpdate() {
-    if(!ticking) {
-      reqAnimationFrame(updateElementTransform);
-      ticking = true;
-    }
-  }
-
-  function logEvent(str) {
-    //log.insertBefore(document.createTextNode(str +"\n"), log.firstChild);
-  }
-
-  function onPan(ev) {
-    transform.translate = {
-      x: ev.deltaX,
-      y: ev.deltaY
-    };
-
-    requestElementUpdate();
-    logEvent(ev.type);
-  }
-
-  var initScale = 1;
-  function onPinch(ev) {
-      if(ev.type == 'pinchstart') {
-          initScale = transform.scale || 1;
-      }
-
-      el.className = '';
-      transform.scale = initScale * ev.scale;
-
-      requestElementUpdate();
-      logEvent(ev.type);
-  }
-
-  var initAngle = 0;
-  function onRotate(ev) {
-      if(ev.type == 'rotatestart') {
-          initAngle = transform.angle || 0;
-      }
-
-      el.className = '';
-      transform.rz = 1;
-      transform.angle = initAngle + ev.rotation;
-      requestElementUpdate();
-      logEvent(ev.type);
-  }
-
-  function onSwipe(ev) {
-      var angle = 50;
-      transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
-      transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
-      transform.angle = (ev.direction & (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
-
-      clearTimeout(timer);
-      timer = setTimeout(function () {
-          syncElement();
-      }, 300);
-      requestElementUpdate();
-      logEvent(ev.type);
-  }
-
-  function onTap(ev) {
-      transform.rx = 1;
-      transform.angle = 25;
-
-      clearTimeout(timer);
-      timer = setTimeout(function () {
-          syncElement();
-      }, 200);
-      requestElementUpdate();
-      logEvent(ev.type);
-  }
-
-  function onDoubleTap(ev) {
-      transform.rx = 1;
-      transform.angle = 80;
-
-      clearTimeout(timer);
-      timer = setTimeout(function () {
-          syncElement();
-      }, 500);
-      requestElementUpdate();
-      logEvent(ev.type);
-  }
-
+  /**
+   * Socket: listen for this shape to move.
+   */
   socket.on('move', function(props) {
     if (props.me !== socket.id) {
+      var iAmHere = extract(props.transform);
+
       $('#' + props.id).css({
         transform: props.transform
       });
+
+      transform.x = iAmHere.x;
+      transform.y = iAmHere.y;
     }
   });
+
+  /**
+   * Hammer: listen for pan
+   */
+  function onPan(ev) {
+    if (ev.type == 'panstart') {
+      initX = transform.x || 0;
+      initY = transform.y || 0;
+    }
+
+    transform.x = initX + ev.deltaX;
+    transform.y = initY + ev.deltaY;
+
+    requestElementUpdate();
+  }
+
+  /**
+   * Hammer: listen for pinch
+   */
+  function onPinch(ev) {
+    if (ev.type == 'pinchstart') {
+      initScale = transform.scale || 1;
+    }
+
+    transform.scale = initScale * ev.scale;
+
+    requestElementUpdate();
+  }
+
+  /**
+   * Hammer: listen for rotate
+   */
+  function onRotate(ev) {
+    if (ev.type == 'rotatestart') {
+      initAngle = transform.angle || 0;
+    }
+
+    transform.rz = 1;
+    transform.angle = initAngle + ev.rotation;
+    requestElementUpdate();
+  }
+
+  /**
+   * Hammer: listen for tap
+   */
+  function onTap(ev) {
+    transform.rx = 1;
+    transform.angle = 25;
+
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      requestElementUpdate();
+    }, 200);
+    requestElementUpdate();
+  }
+
+  /**
+   * Hammer: listen for double tap
+   */
+  function onDoubleTap(ev) {
+    transform.rx = 1;
+    transform.angle = 80;
+
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      requestElementUpdate();
+    }, 500);
+    requestElementUpdate();
+  }
 });
