966ac84edb60820cbc49167c832f4378c3dce3a5
Chris Ruppel <chris.ruppel@gmail.com>
2015-06-18 15:21:44 +0200

    :construction: try to use heroku's port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 // Main app URL
 app.get('/', function(req, res){
@@ -47,6 +48,6 @@ io.on('connection', function(socket){
 /**
  * Listen for users to connect
  */
-http.listen(3000, function(){
-  console.log('Listening on port 3000');
+http.listen(port, function(){
+  console.log('Listening on port ' + port);
 });
