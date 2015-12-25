cb33ce2ab17f2ef33b0ff180f4b21df898297a32
Chris Ruppel <chris.ruppel@gmail.com>
2015-12-25 16:14:06 +0700

    :nail_care: add a cross-browser color picker

--- js/controls.js
+++ js/controls.js
@@ -7,25 +7,39 @@ var controls = $$('#form-controls label');
 controls.forEach(function(current, index, array) {
   current.on('touchend', function debugLabel() {
     // Debug
-    console.info('You touched ' + current.htmlFor);
+    // console.info('You touched ' + current.htmlFor);
 
     // Shut the current visible widget if needed.
     $$('#form-controls .visible').forEach(function (el, i) {
       el.classList.remove('visible');
-      console.info(el);
     });
 
     // Whitelist which widgets can pop up.
-    var toggle_widgets = ['opacity', 'color'];
+    var toggle_widgets = ['opacity'];
 
     // Toggle the widget if needed.
     if (toggle_widgets.indexOf(current.htmlFor) !== -1) {
       $('#' + current.htmlFor).classList.toggle('visible');
 
       // Remove focus from color input when closing.
-      if (current.htmlFor === 'color' && !$('#' + current.htmlFor).classList.contains('visible')) {
+      if (current.htmlFor === 'color') {
         blurAll();
       }
     }
   })
+});
+
+// Enable color picker on color input
+jsColorPicker('#color', {
+  size: 3,
+  fps: 60,
+  noAlpha: true,
+  customBG: '#000',
+  appendTo: $('label[for="color"]'),
+  memoryColors: false,
+  noRangeBackground: false,
+  noResize: false,
+  noRGBr: false,
+  noRGBg: false,
+  noRGBb: false,
 });
