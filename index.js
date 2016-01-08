rupl/bustashape

commit 37e0edca6957712a8e5241ad61848e7dc34434a7

:hammer: use consistent syntax when emitting events

index.js
@@ -98,7 +98,7 @@ io.on('connection', function(socket){
     socket.join(roomName);
 
     // Tell others that this person joined.
-    socket.broadcast.in(roomName).emit('user-join', {
+    socket.broadcast.to(socket.room).emit('user-join', {
       'nick': client.nick,
       'sid' : client.sid
     });
@@ -118,7 +118,7 @@ io.on('connection', function(socket){
    */
   socket.on('add', function(props){
     console.log('ADD', socket.room, props);
-    io.to(socket.room).emit('add', props);
+    socket.broadcast.to(socket.room).emit('add', props);
   });
 
   /**

commit a07570871b0e440ea9146cd3dcfa33ee85d62c80

:bug: Revert socket.io syntax for adding shapes
Fixes bug introduced in 37e0edca

index.js
@@ -118,7 +118,9 @@ io.on('connection', function(socket){
    */
   socket.on('add', function(props){
     console.log('ADD', socket.room, props);
-    socket.broadcast.to(socket.room).emit('add', props);
+    // Don't use socket.broadcast because the user who initiated the event needs
+    // to receive it in the same fashion as the others in the room.
+    io.to(socket.room).emit('add', props);
   });
 
   /**