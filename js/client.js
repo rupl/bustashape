ab478c75a0f1910084d21e3b02466a24ed877bad
Chris Ruppel <chris.ruppel@gmail.com>
2016-04-20 10:27:42 +0200

    :nail_care: make new rectangles entirely visible in default zoom/pan state

--- js/client.js
+++ js/client.js
@@ -33,7 +33,7 @@ socket.on('add', function(props) {
   if (props.class === 'circle') {
     var shape = two.makeCircle(START_X, START_Y, START_RADIUS);
   } else if (props.class === 'rectangle') {
-    var shape = two.makeRectangle(START_X, START_Y, START_WIDTH*2, START_HEIGHT);
+    var shape = two.makeRectangle(START_X*2, START_Y, START_WIDTH*2, START_HEIGHT);
   } else if (props.class === 'triangle') {
     var shape = two.makePolygon(START_X, START_Y, START_WIDTH/1.5, 3);
   } else {
