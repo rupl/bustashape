d9dcb3bcde18dc1b127c72ac25ea46e30f7879d5
Chris Ruppel <chris.ruppel@gmail.com>
2016-04-27 14:17:41 +0200

    :construction: move Math utils into _utils.js

--- js/client.js
+++ js/client.js
@@ -250,13 +250,3 @@ socket.on('add', function(props) {
     }
   });
 });
-
-// Converts from degrees to radians.
-Math.radians = function(degrees) {
-  return degrees * Math.PI / 180;
-};
-
-// Converts from radians to degrees.
-Math.degrees = function(radians) {
-  return radians * 180 / Math.PI;
-};
--- js/utils.js
+++ js/utils.js
@@ -56,6 +56,18 @@ function n(x) {
   return Number(x);
 }
 
+
+//
+// Math stuff
+//
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};
+
+
 //
 // Debug functions
 //
